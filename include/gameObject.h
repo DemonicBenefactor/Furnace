d102b329c1c7b2feaf77e6907db5b3d455e2d29e
demonicbenefactor <demonic@tutanota.de>
2015-12-09 12:20:10 +0200

    gameObject wip

--- include/gameObject.h
+++ include/gameObject.h
@@ -6,11 +6,16 @@
 #include <SDL2/SDL.h>
 #include "textureManager.h"
 
-void FUR_drawObject();
-void FUR_updateObject();
+void FUR_drawObject(SDL_Renderer *pRenderer, gameObject *pGameObject);
+void FUR_updateObject(gameObject *pGameObject);
 void FUR_cleanObject();
 
 typedef struct {
+	int textureID;	
+	int currentRow;
+	int currentFrame;
+	int width;
+	int height;
 	int x;
 	int y;
 } gameObject;
