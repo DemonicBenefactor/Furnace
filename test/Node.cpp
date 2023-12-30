6d7523db7067eacb0e9dc81c8e71ec8fdf85050b
DemonicBenefactor <demonic@tutanota.de>
2023-12-30 12:16:21 +0200

    modified:   test/Node.cpp 	modified:   test/Node.hpp

--- test/Node.cpp
+++ test/Node.cpp
@@ -1,15 +1,17 @@
 #include "Node.hpp"
 #include <iostream>
+#include <array>
 
 using namespace std;
 
 int main(int argc, char **argv) {
+  array<double, 2> position = {3.333, -4.567};
+  double &x = position[0];
+  double &y = position[1];
+  array<double, 2> &xy = position;
 
-Node2D n1;
-Node2D n2(5.3, 3.7);
 
-
-
+  cout << xy[0] << endl;
 
   return 0;
 }
--- test/Node.hpp
+++ test/Node.hpp
@@ -1,8 +1,8 @@
 #ifndef __Node__
 #define __Node__
 
-#include <string>
 #include <memory>
+#include <string>
 
 using namespace std;
 
@@ -20,18 +20,19 @@ protected:
 
 class Node2D : Node {
 public:
-  Node2D() : x(0), y(0) {}
-  Node2D(double ix, double iy) : x(ix), y(iy) {}
-  Node2D(const Node2D &) {}
+  Node2D() : mX(0), mY(0), x(mX), y(mY) {}
+  Node2D(double ix, double iy) : mX(ix), mY(iy), x(mX), y(mY) {}
+  // Node2D(const Node2D &) {}
   ~Node2D() {}
 
   Node2D &operator=(const Node2D &) { return *this; }
 
   virtual void update() {}
 
-  double x, y;
+  double &x, &y;
 
 private:
+  double mX, mY;
 };
 
 /*-----------DERIVED CLASS----------*/
